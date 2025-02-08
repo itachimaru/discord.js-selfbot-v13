cf28d9b4e29e9e5a6e05267da5363dd3b83a2b2e
itachimaru <depressive.suicidal.bm@gmail.com>
2025-02-08 12:05:33 +0100

    Update RichPresence.js

--- examples/RichPresence.js
+++ examples/RichPresence.js
@@ -6,7 +6,7 @@ client.on('ready', async () => {
   const getExtendURL = await RichPresence.getExternal(
     client,
     '367827983903490050',
-    'https://assets.ppy.sh/beatmaps/1550633/covers/list.jpg', // Required if the image you use is not in Discord
+    'https://c.tenor.com/4QYDTI8lVcUAAAAd/tenor.gif', // Required if the image you use is not in Discord
   );
   const status = new RichPresence(client)
     .setApplicationId('367827983903490050')
